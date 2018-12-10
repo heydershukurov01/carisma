d5e3f2746e84fc8f708bc0a7a064b89ca42ef17a
h.shukurov <heyder.s@coded.edu.az>
2018-12-09 22:42:26 -0800

    Changes

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -7,7 +7,7 @@
             <div class="form-group"><label for="">Şifrə:</label><input name="password" type="password" class="form-control" required></div>
             <hr class="my-4">
             <div class="alert alert-info">İştirakçıların adlarını, korporativ e-poçt ünvanlarını, və şifrəsini qeyd edin və Əlavə et düyməsini sıxın!</div>
-            <button type="submit" class="btn btn-primary btn-lg" href="#" role="button">Əlavə et</button>
+            <button type="submit" class="btn btn-primary btn-lg" href="#" role="button">Əlavə et!</button>
         </form>
     </div>
 </div>
